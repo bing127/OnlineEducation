a86cd3994d262745fa4159f0e5ac818494207ae4
bing127 <wxbing1207@163.com>
2020-08-11 00:34:06 +0800

    add 依赖

--- app/oe/lib/app/init_page.dart
+++ app/oe/lib/app/init_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:oe/app/start_page.dart';
 
 class InitPage extends StatefulWidget {
@@ -10,7 +11,9 @@ class _InitPageState extends State<InitPage> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: false);
     return MaterialApp(
+      navigatorKey: GlobalKey(debugLabel: "oe_app_global_key"),
       title: 'OnlineEducation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
