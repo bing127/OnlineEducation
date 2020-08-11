f24a002308f76ab69993caed63da66995e3a3c0b
wxb <wxbing1207@163.com>
2020-08-11 21:27:02 +0800

    init start page

--- app/oe/lib/app/init_page.dart
+++ app/oe/lib/app/init_page.dart
@@ -1,25 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:oe/app/start_page.dart';
+import 'package:oe/router/application.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-class InitPage extends StatefulWidget {
-  @override
-  _InitPageState createState() => _InitPageState();
-}
-
-class _InitPageState extends State<InitPage> {
+class InitPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: false);
     return MaterialApp(
       navigatorKey: GlobalKey(debugLabel: "oe_app_global_key"),
       title: 'OnlineEducation',
+      showPerformanceOverlay: false, //显示性能标签
+      debugShowCheckedModeBanner: false,
+      checkerboardRasterCacheImages: false,
+      checkerboardOffscreenLayers: false,
+      showSemanticsDebugger: false, // 显示语义视图
+      onGenerateRoute: Application.router.generator,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: StartPage(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('zh', 'CH'),
+        Locale('en', 'US')
+      ],
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0), // 或者 MediaQueryData.fromWindow(WidgetsBinding.instance.window).copyWith(textScaleFactor: 1.0),
+          child: child,
+        );
+      },
+      locale: const Locale("en", "US"),
+      localeResolutionCallback: (Locale locale, Iterable<Locale> supportedLocales) {
+        return locale;
+      },
     );
   }
 }
--- app/oe/lib/main.dart
+++ app/oe/lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/screenutil.dart';
+import 'dart:io';
+import 'package:sp_util/sp_util.dart';
 
 import 'package:oe/app/init_page.dart';
-void main() {
+import 'package:oe/app/exception_page.dart';
+void main() async{
+  ScreenUtil.init(designSize: Size(750, 1334), allowFontScaling: false);
+  /// 确保初始化
+  WidgetsFlutterBinding.ensureInitialized();
+  /// 配置初始化
+  await SpUtil.getInstance();
+  /// 自定义报错页面
+  ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails){
+    print(flutterErrorDetails.toString());
+    return ExceptionPage();
+  };
+  // 沉浸式状态栏
+  if (Platform.isAndroid) {
+    SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
   runApp(InitPage());
 }
