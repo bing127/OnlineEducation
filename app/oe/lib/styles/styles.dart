d19767aa7d5ca545412044e73547b7ba3c824b3a
wxb <wxbing1207@163.com>
2020-08-11 21:56:54 +0800

    新增styles

---
+++ app/oe/lib/styles/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const firstColor = Color(0xff28B28B);
+  static const secondColor = Color(0xfffe2b67);
+  static const thirdColor = Color(0xfffdbb01);
+  static const titleColor = Color(0xff191A1A);
+  static const sub0Color = Color(0xff6F7172);
+  static const sub1Color = Color(0xff9A9B9D);
+  static const sub2Color = Color(0xffDBDDDF);
+}
